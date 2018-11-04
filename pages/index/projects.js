517d8bd586134e9944abfab73ae35d9d52f36937
Igor Razvodov <bnsaintz@gmail.com>
2018-11-04 19:03:52 +0300

    enhanced animation perfomance by throttling scroll event listener

--- pages/index/projects.js
+++ pages/index/projects.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import styled, { keyframes } from 'styled-components'
 import throttle from 'lodash.throttle'
 import debounce from 'lodash.debounce'
@@ -36,8 +36,9 @@ const ProjectInfo = styled.div`
   padding: 80px 0;
   transition: 0.4s;
   transition-timing-function: ease-in-out;
-  opacity: ${({ active }) => active ? 1 : 0};
-  transform: ${({ active }) => active ? 'translate3d(0, 0, 0)' : 'translate3d(0, 200px, 0)'};
+
+  /* opacity: ${({ active }) => active ? 1 : 0};
+  transform: ${({ active }) => active ? 'translate3d(0, 0, 0)' : 'translate3d(0, 200px, 0)'}; */
 `
 
 const ProjectTitle = styled.h4`
@@ -64,48 +65,57 @@ export default class Projects extends React.Component {
     project2IsActive: false,
     project1IsActive: false
   }
-  projects = React.createRef();
-  project1 = React.createRef();
-  project2 = React.createRef();
+  projects = createRef();
+  project1 = createRef();
+  project2 = createRef();
+  projectInfo1 = createRef();
+  projectInfo2 = createRef();
+  throttledScrollHandler = throttle(this.handleScroll, 100).bind(this);
+
 
   componentDidMount() {
-    document.addEventListener('scroll', throttle(this.handleScroll, 200));
-    this.handleScroll();
+    document.addEventListener('scroll', this.throttledScrollHandler, false)
   }
 
   componentWillUnmount() {
-    document.removeEventListener('scroll', throttle(this.handleScroll, 200));
+    document.removeEventListener('scroll', this.throttledScrollHandler, false);
   }
 
-  handleScroll = event => {
-  
-    const windowHeight = window.innerHeight;
+  handleScroll() {
+    const projects = this.projects.current;
     const project1 = this.project1.current;
     const project2 = this.project2.current;
+    const projectInfo1 = this.projectInfo1.current;
+    const projectInfo2 = this.projectInfo2.current;
     const project1OffsetTop = project1.getBoundingClientRect().top;
     const project2OffsetTop = project2.getBoundingClientRect().top;
+    window.requestAnimationFrame(() => {
+      if (project1OffsetTop < 150) {
+        projectInfo1.style.opacity = 1;
+        projectInfo1.style.transform = 'translate3d(0,0,0)';
+        projects.style.backgroundColor = '#374140';
+      } else {
+        projectInfo1.style.opacity = 0;
+        projectInfo1.style.transform = 'translate3d(0,200px,0)';
+        projects.style.backgroundColor = '#2A2C2B';
+      }
+      if (project2OffsetTop < 150) {
+        projectInfo2.style.opacity = 1;
+        projectInfo2.style.transform = 'translate3d(0,0,0)';
+      } else {
+        projectInfo2.style.opacity = 0;
+        projectInfo2.style.transform = 'translate3d(0,200px,0)';
+      }
+    })
 
-    if(project1OffsetTop < 150){
-      this.setState({ project1IsActive: true })
-    } else {
-      this.setState({ project1IsActive: false })
-    }
-    if (project2OffsetTop < 150) {
-      this.setState({ project2IsActive: true })
-      this.projects.current.style.backgroundColor = '#374140';
-    } else {
-      this.setState({ project2IsActive: false })
-      this.projects.current.style.backgroundColor = '#2A2C2B';
-    }
   }
 
   render() {
     return (
       <ProjectsWrapper ref={this.projects}>
-
         <Project ref={this.project1}>
           <ProjectImage src='/static/mobile.png' />
-          <ProjectInfo active={this.state.project1IsActive}>
+          <ProjectInfo ref={this.projectInfo1}>
             <ProjectTitle>
               Project 1
             </ProjectTitle>
@@ -117,21 +127,20 @@ export default class Projects extends React.Component {
             </ProjectDescription>
           </ProjectInfo>
         </Project>
-          <Project ref={this.project2}>
-            <ProjectImage src='/static/mobile2.png' />
-            <ProjectInfo
-              active={this.state.project2IsActive}>
-              <ProjectTitle>
-                Project 1
+        <Project ref={this.project2}>
+          <ProjectImage src='/static/mobile2.png' />
+          <ProjectInfo ref={this.projectInfo2}>
+            <ProjectTitle>
+              Project 1
             </ProjectTitle>
-              <ProjectDescription>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis impedit odit earum enim molestias dolorum porro facere dolore alias, id modi cumque magnam quaerat perspiciatis, aliquid doloribus? Maiores, doloremque ipsam!
+            <ProjectDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis impedit odit earum enim molestias dolorum porro facere dolore alias, id modi cumque magnam quaerat perspiciatis, aliquid doloribus? Maiores, doloremque ipsam!
             </ProjectDescription>
-              <ProjectDescription>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis impedit odit earum enim molestias dolorum porro facere dolore alias, id modi cumque magnam quaerat perspiciatis, aliquid doloribus? Maiores, doloremque ipsam!
+            <ProjectDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis impedit odit earum enim molestias dolorum porro facere dolore alias, id modi cumque magnam quaerat perspiciatis, aliquid doloribus? Maiores, doloremque ipsam!
             </ProjectDescription>
-            </ProjectInfo>
-          </Project>
+          </ProjectInfo>
+        </Project>
       </ProjectsWrapper>
     )
   }
